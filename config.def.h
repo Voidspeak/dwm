2bfc3ab6d3d9f242940f3eff2e90decb3a64954a
Predrag Perisic <predrag3.14159@gmail.com>
2021-02-10 14:14:52 +0100

    added rules for steam and copyq

--- config.def.h
+++ config.def.h
@@ -31,9 +31,10 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/* class      instance    title           tags mask     isfloating   monitor */
+	{ "Steam",    NULL,       NULL,           1 << 8,       1,           -1 },
+	{ NULL,       NULL,       "Steam",        1 << 8,       1,           -1 },
+	{ "copyq",    NULL,       NULL,           0,            1,           -1 },
 };
 
 /* layout(s) */
