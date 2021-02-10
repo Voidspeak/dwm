b4dfaa0fe5faef53afb975dd613155cef5177d56
Predrag Perisic <predrag3.14159@gmail.com>
2021-02-10 14:17:23 +0100

    added lock functionality

--- config.def.h
+++ config.def.h
@@ -64,11 +64,15 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_burgundy, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "konsole", NULL };
+static const char *lockcmd[]    = { "slock", NULL };
+//static const char *sleepcmd[]   = { "slock", "sudo", "/usr/sbin/s2ram", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_F12,    spawn,          {.v = lockcmd } },
+//	{ MODKEY|ShiftMask,             XK_F12,    spawn,          {.v = sleepcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
