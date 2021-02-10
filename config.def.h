47327e6b7b21560405f278c89074e64cfa9659aa
Predrag Perisic <predrag3.14159@gmail.com>
2021-02-10 13:36:06 +0100

    changed fonts

--- config.def.h
+++ config.def.h
@@ -5,8 +5,13 @@ static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = {
+	"Iosevka:style=Regular:size=13",
+	"Font Awesome 5 Free,Font Awesome 5 Free Solid:style=Solid:pixelsize=16",
+	"Font Awesome 5 Free,Font Awesome 5 Free Regular:style=Regular:pixelsize=16",
+	"Font Awesome 5 Brands,Font Awesome 5 Brands Regular:style=Regular:pixelsize=16"
+};
+static const char dmenufont[]       = "monospace:size=14";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
